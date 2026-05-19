4797e46464909e19dad7c1974a74567630d4dbbb
DuKopper <dudukopper@gmail.com>
2026-05-19 16:22:36 -0300

    Tarefa - Adicionando Texturas com arquivo .mtl

---
+++ Tarefa - Adicionando Texturas/Hello3D.cpp
@@ -0,0 +1,486 @@
+/* Hello Triangle - código adaptado de https://learnopengl.com/#!Getting-started/Hello-Triangle
+ *
+ * Adaptado por Rossana Baptista Queiroz
+ * para as disciplinas de Processamento Gráfico/Computação Gráfica - Unisinos
+ * Versão inicial: 7/4/2017
+ * Última atualização em 07/03/2025
+ */
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include "stb_image.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+// GLAD
+#include <glad/glad.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int loadSimpleOBJ(string filePATH, int &nVertices)
+{
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec2> texCoords;
+	std::vector<glm::vec3> normals;
+	std::vector<GLfloat> vBuffer;
+
+	std::ifstream arqEntrada(filePATH.c_str());
+	if (!arqEntrada.is_open())
+	{
+		std::cerr << "Erro ao tentar ler o arquivo " << filePATH << std::endl;
+		return -1;
+	}
+
+	std::string line;
+	while (std::getline(arqEntrada, line))
+	{
+		std::istringstream ssline(line);
+		std::string word;
+		ssline >> word;
+
+		if (word == "v")
+		{
+			glm::vec3 vertice;
+			ssline >> vertice.x >> vertice.y >> vertice.z;
+			vertices.push_back(vertice);
+		}
+		else if (word == "vt")
+		{
+			glm::vec2 vt;
+			ssline >> vt.s >> vt.t;
+			texCoords.push_back(vt);
+		}
+		else if (word == "vn")
+		{
+			glm::vec3 normal;
+			ssline >> normal.x >> normal.y >> normal.z;
+			normals.push_back(normal);
+		}
+		else if (word == "f")
+		{
+			while (ssline >> word)
+			{
+				int vi = 0, ti = 0, ni = 0;
+				std::istringstream ss(word);
+				std::string index;
+
+				if (std::getline(ss, index, '/'))
+					vi = !index.empty() ? std::stoi(index) - 1 : 0;
+				if (std::getline(ss, index, '/'))
+					ti = !index.empty() ? std::stoi(index) - 1 : 0;
+				if (std::getline(ss, index))
+					ni = !index.empty() ? std::stoi(index) - 1 : 0;
+
+				vBuffer.push_back(vertices[vi].x);
+				vBuffer.push_back(vertices[vi].y);
+				vBuffer.push_back(vertices[vi].z);
+
+				vBuffer.push_back(texCoords[ti].x);
+				vBuffer.push_back(texCoords[ti].y);
+			}
+		}
+	}
+
+	arqEntrada.close();
+
+	std::cout << "Gerando o buffer de geometria..." << std::endl;
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vBuffer.size() * sizeof(GLfloat), vBuffer.data(), GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,
+						  5 * sizeof(GLfloat),
+						  (GLvoid *)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(
+		0,
+		3,
+		GL_FLOAT,
+		GL_FALSE,
+		5 * sizeof(GLfloat),
+		(GLvoid *)0);
+
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	nVertices = vBuffer.size() / 5;
+
+	return VAO;
+}
+
+GLuint loadTexture(const char *path)
+{
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	// parâmetros
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D,
+					GL_TEXTURE_MIN_FILTER,
+					GL_LINEAR);
+
+	glTexParameteri(GL_TEXTURE_2D,
+					GL_TEXTURE_MAG_FILTER,
+					GL_LINEAR);
+
+	int width, height, nrChannels;
+
+	unsigned char *data =
+		stbi_load(path, &width, &height,
+				  &nrChannels, 0);
+
+	if (data)
+	{
+		GLenum format;
+
+		if (nrChannels == 3)
+			format = GL_RGB;
+		else
+			format = GL_RGBA;
+
+		glTexImage2D(GL_TEXTURE_2D,
+					 0,
+					 format,
+					 width,
+					 height,
+					 0,
+					 format,
+					 GL_UNSIGNED_BYTE,
+					 data);
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+
+	stbi_image_free(data);
+
+	return textureID;
+}
+
+GLuint loadTextureFromMTL(std::string mtlPath)
+{
+    std::ifstream file(mtlPath);
+
+    if (!file.is_open())
+    {
+        std::cout << "Erro ao abrir MTL\n";
+        return 0;
+    }
+
+    fs::path baseDir = fs::path(mtlPath).parent_path();
+
+    std::string line;
+
+    while (getline(file, line))
+    {
+        std::istringstream ss(line);
+
+        std::string word;
+        ss >> word;
+
+        if (word == "map_Kd")
+        {
+            std::string textureFile;
+            ss >> textureFile;
+
+            fs::path fullPath = baseDir / textureFile;
+
+            std::cout << "Textura encontrada: "
+                      << fullPath << std::endl;
+
+            return loadTexture(fullPath.string().c_str());
+        }
+    }
+
+    return 0;
+}
+
+struct OBJ
+{
+	GLuint VAO;
+	int nVertices;
+	GLuint textureID;
+
+	glm::vec3 position;
+	glm::vec3 rotationAxis;
+	float angle;
+	glm::vec3 scale;
+	bool rotating;
+};
+
+std::vector<OBJ> objects;
+
+int selectedObject = 0;
+
+// Protótipo da função de callback de teclado
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+// Protótipos das funções
+int setupShader();
+int setupGeometry();
+
+// Dimensões da janela (pode ser alterado em tempo de execução)
+const GLuint WIDTH = 1000, HEIGHT = 1000;
+
+// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
+const GLchar *vertexShaderSource = "#version 330 core\n"
+								   "layout (location = 0) in vec3 position;\n"
+								   "layout (location = 1) in vec2 texCoord;\n"
+								   "uniform mat4 model;\n"
+								   "out vec2 TexCoord;;\n"
+								   "void main()\n"
+								   "{\n"
+								   "gl_Position = model * vec4(position, 1.0);\n"
+								   "TexCoord = texCoord;\n"
+								   "}\0";
+
+// Códifo fonte do Fragment Shader (em GLSL): ainda hardcoded
+const GLchar *fragmentShaderSource = "#version 330 core\n"
+									 "in vec2 TexCoord;\n"
+									 "out vec4 color;\n"
+									 "uniform sampler2D texture1;\n"
+									 "void main()\n"
+									 "{\n"
+									 "color = texture(texture1, TexCoord);\n"
+									 "}\n\0";
+
+// Função MAIN
+int main()
+{
+	// Inicialização da GLFW
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Criação da janela GLFW
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Ola 3D -- Eduardo!", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	// Fazendo o registro da função de callback para a janela GLFW
+	glfwSetKeyCallback(window, key_callback);
+
+	// GLAD: carrega todos os ponteiros d funções da OpenGL
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+	}
+
+	// Obtendo as informações de versão
+	const GLubyte *renderer = glGetString(GL_RENDERER);
+	const GLubyte *version = glGetString(GL_VERSION);
+	cout << "Renderer: " << renderer << endl;
+	cout << "OpenGL version supported " << version << endl;
+
+	// Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	// Compilando e buildando o programa de shader
+	GLuint shaderID = setupShader();
+
+	// Gerando um buffer simples
+	int nVertices;
+	GLuint objVAO = loadSimpleOBJ("C:\\Users\\user\\Documents\\Objetos 3D\\Suzanne.obj", nVertices);
+	GLuint texture = loadTextureFromMTL("C:\\Users\\user\\Documents\\Texturas\\Suzanne.mtl");
+
+	OBJ obj1;
+	obj1.textureID = texture;
+	obj1.VAO = objVAO;
+	obj1.nVertices = nVertices;
+	obj1.position = glm::vec3(-0.5f, 0.0f, 0.0f);
+	obj1.rotationAxis = glm::vec3(1.0f, 0.0f, 0.0f);
+	obj1.rotating = false;
+	obj1.angle = 0.0f;
+	obj1.scale = glm::vec3(0.3f);
+
+	OBJ obj2 = obj1;
+	obj2.textureID = texture;
+	obj2.position = glm::vec3(0.5f, 0.0f, 0.0f);
+	obj2.rotationAxis = glm::vec3(0.0f, 1.0f, 0.0f);
+	obj2.rotating = false;
+
+	objects.push_back(obj1);
+	objects.push_back(obj2);
+
+	glUseProgram(shaderID);
+	glUniform1i(glGetUniformLocation(shaderID, "texture1"), 0);
+
+	glm::mat4 model = glm::mat4(1); // matriz identidade;
+	GLint modelLoc = glGetUniformLocation(shaderID, "model");
+	//
+	model = glm::rotate(model, /*(GLfloat)glfwGetTime()*/ glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	glEnable(GL_DEPTH_TEST);
+
+	// Loop da aplicação - "game loop"
+	while (!glfwWindowShouldClose(window))
+	{
+		// Checa se houveram eventos de input (key pressed, mouse moved etc.) e chama as funções de callback correspondentes
+		glfwPollEvents();
+
+		// Limpa o buffer de cor
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // preto
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glLineWidth(10);
+		glPointSize(20);
+
+		for (auto &obj : objects)
+		{
+			if (obj.rotating)
+				obj.angle += 0.01f;
+
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, obj.position);
+			model = glm::rotate(model, obj.angle, obj.rotationAxis);
+			model = glm::scale(model, obj.scale);
+
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+			glBindVertexArray(obj.VAO);
+
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, obj.textureID);
+
+			glDrawArrays(GL_TRIANGLES, 0, obj.nVertices);
+		}
+
+		// Troca os buffers da tela
+		glfwSwapBuffers(window);
+	}
+	// Pede pra OpenGL desalocar os buffers
+	glDeleteVertexArrays(1, &objVAO);
+	// Finaliza a execução da GLFW, limpando os recursos alocados por ela
+	glfwTerminate();
+	return 0;
+}
+
+// Função de callback de teclado - só pode ter uma instância (deve ser estática se
+// estiver dentro de uma classe) - É chamada sempre que uma tecla for pressionada
+// ou solta via GLFW
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	OBJ &obj = objects[selectedObject];
+
+	// MOVIMENTO
+	if (key == GLFW_KEY_W && (action == GLFW_PRESS || action == GLFW_REPEAT))
+		obj.position.y += 0.1f;
+
+	if (key == GLFW_KEY_S && (action == GLFW_PRESS || action == GLFW_REPEAT))
+		obj.position.y -= 0.1f;
+
+	if (key == GLFW_KEY_A && (action == GLFW_PRESS || action == GLFW_REPEAT))
+		obj.position.x -= 0.1f;
+
+	if (key == GLFW_KEY_D && (action == GLFW_PRESS || action == GLFW_REPEAT))
+		obj.position.x += 0.1f;
+
+	// ESCALA
+	if (key == GLFW_KEY_LEFT_BRACKET && (action == GLFW_PRESS || action == GLFW_REPEAT))
+		obj.scale -= glm::vec3(0.1f);
+
+	if (key == GLFW_KEY_RIGHT_BRACKET && (action == GLFW_PRESS || action == GLFW_REPEAT))
+		obj.scale += glm::vec3(0.1f);
+
+	if (obj.scale.x < 0.1f || obj.scale.y < 0.1f || obj.scale.z < 0.1f)
+		obj.scale = glm::vec3(0.1f);
+
+	// ROTAÇÃO
+	if (key == GLFW_KEY_X && action == GLFW_PRESS)
+		objects[selectedObject].rotationAxis = glm::vec3(1, 0, 0);
+
+	if (key == GLFW_KEY_Y && action == GLFW_PRESS)
+		objects[selectedObject].rotationAxis = glm::vec3(0, 1, 0);
+
+	if (key == GLFW_KEY_Z && action == GLFW_PRESS)
+		objects[selectedObject].rotationAxis = glm::vec3(0, 0, 1);
+
+	// Seleção de objeto em cena
+	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
+		selectedObject = 0;
+
+	if (key == GLFW_KEY_2 && action == GLFW_PRESS)
+		selectedObject = 1;
+
+	if (key == GLFW_KEY_R && action == GLFW_PRESS)
+	{
+		objects[selectedObject].rotating = !objects[selectedObject].rotating;
+	}
+}
+
+int setupShader()
+{
+	// Vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Checando erros de compilação (exibição via log no terminal)
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	// Fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Checando erros de compilação (exibição via log no terminal)
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	// Linkando os shaders e criando o identificador do programa de shader
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Checando por erros de linkagem
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+				  << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
+}
